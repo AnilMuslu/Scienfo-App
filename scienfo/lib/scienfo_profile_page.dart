b988288bf8ba046a6d60a02aa3620e5cf2ddda16
cagatay00 <jacktherhycer@gmail.com>
2023-07-02 14:18:37 +0300

    "profile page updated"

--- scienfo/lib/scienfo_profile_page.dart
+++ scienfo/lib/scienfo_profile_page.dart
@@ -55,113 +55,189 @@ class _ScienfoProfilePageState extends State<ScienfoProfilePage> {
         children: <Widget>[
           Pinned.fromPins(
             Pin(size: 150.0, middle: 0.5),
-            Pin(size: 153.4, start: 106.6),
+            Pin(size: 153.4, start: 54.6),
             child: UserImage(
                 uid: FirebaseAuth.instance.currentUser?.uid ??
                     'defaultUid'), // Add the check here
+          ),
+          Pinned.fromPins(
+            Pin(start: 10.0, end: 10.0),
+            Pin(size: 50.0, middle: 0.25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                        right: 10.0), // Spacing between the line and the text
+                    height:
+                        2.0, // Adjust the height to control the thickness of the line
+                    color: Colors.black, // Color of the line
+                  ),
+                ),
+                Text(
+                  'Profile',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    color: const Color(0xff000000),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                        left: 10.0), // Spacing between the line and the text
+                    height:
+                        2.0, // Adjust the height to control the thickness of the line
+                    color: Colors.black, // Color of the line
+                  ),
+                ),
+              ],
+            ),
           ),
           Pinned.fromPins(
             Pin(size: 7.0, end: 20.0),
             Pin(size: 34.0, start: 53.0),
             child: ExitIcon(),
           ),
-          Transform.translate(
-            offset: Offset(0.0, 367.0),
-            child: Container(
-              width: 410.0,
-              height: 0.0,
-              color: const Color(0xff707070),
-            ),
-          ),
           Pinned.fromPins(
             Pin(size: 200.0, middle: 0.5),
-            Pin(size: 50.0, middle: 0.5),
+            Pin(size: 50.0, middle: 0.4),
             child: FutureBuilder<String?>(
               future: getUserType(),
               builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
                 final userType = snapshot.data ?? '#General';
 
                 return RichText(
+                  textAlign: TextAlign.center,
                   text: TextSpan(
-                    text: 'Change profile type (current: ',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    text: 'Current Profile Type: ',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                     children: <TextSpan>[
                       TextSpan(
-                          text: userType,
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      TextSpan(text: ')'),
+                        text: userType,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
                     ],
                   ),
                 );
               },
             ),
           ),
-          Pinned.fromPins(
-            Pin(size: 170.0, middle: 0.5),
-            Pin(size: 50.0, middle: 0.7),
-            child: DropdownButton<String>(
-              value: selectedUserType,
-              icon: const Icon(
-                Icons.arrow_downward,
-                color: Colors.white,
-              ),
-              elevation: 16,
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
-              ),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedUserType = newValue!;
-                });
-              },
-              items: <String>[
-                '#Pre-school',
-                '#Primary-school',
-                '#Middle-school',
-                '#General'
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(
-                    value,
-                    style: TextStyle(color: Colors.black),
+          Center(
+            child: Pinned.fromPins(
+              Pin(size: 200.0, middle: 0.5),
+              Pin(
+                  size: 150.0,
+                  middle: 0.6), // Adjust the size to fit both children
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Change Profile Type:",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                );
-              }).toList(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      padding: EdgeInsets.all(3), // adjust for desired padding
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                            color: Colors.black,
+                            width:
+                                1.5), // adjust for desired border color and width
+                        borderRadius: BorderRadius.circular(
+                            4), // optional, for rounded corners
+                      ),
+                      child: DropdownButton<String>(
+                        value: selectedUserType,
+                        icon: const Icon(
+                          Icons.arrow_downward,
+                          color: Colors.black,
+                        ),
+                        elevation: 16,
+                        underline: Container(
+                          height: 2,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            selectedUserType = newValue!;
+                          });
+                        },
+                        items: <String>[
+                          '#Pre-school',
+                          '#Primary-school',
+                          '#Middle-school',
+                          '#General'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    child: Text("Save"),
+                    onPressed: () {
+                      updateUserType(FirebaseAuth.instance.currentUser!.uid,
+                          selectedUserType);
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Notification'),
+                            content: Text('User type saved'),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            ScienfoContentPage1()),
+                                  );
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
-          Pinned.fromPins(
-            Pin(size: 200.0, middle: 0.5),
-            Pin(size: 50.0, middle: 0.8),
-            child: ElevatedButton(
-              child: Text("Save"),
-              onPressed: () {
-                updateUserType(
-                    FirebaseAuth.instance.currentUser!.uid, selectedUserType);
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Notification'),
-                      content: Text('User type saved'),
-                      actions: <Widget>[
-                        TextButton(
-                          child: Text('OK'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ScienfoContentPage1()),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
+          Align(
+            alignment:
+                Alignment(0.0, 0.8), // adjust the alignment values as necessary
+            child: Container(
+              width: 140.0, // adjust the size as necessary
+              height: 140.0, // adjust the size as necessary
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image:
+                      const AssetImage('assets/images/scienfo_logo_image.png'),
+                  fit: BoxFit.fill,
+                ),
+                borderRadius: BorderRadius.circular(56.0),
+              ),
             ),
           ),
           Pinned.fromPins(
--- scienfo/lib/user_image.dart
+++ scienfo/lib/user_image.dart
@@ -27,62 +27,48 @@ class _UserImageState extends State<UserImage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<String?>(
-      future: userEmail,
-      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text("Error: ${snapshot.error}");
-        } else {
-          return Stack(
-            children: <Widget>[
-              
-              Transform.translate(
-                offset: Offset(14.9, 0.0),
-                child: SizedBox(
-                  width: 120.0,
-                  height: 122.0,
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: 120.0,
-                        height: 122.0,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: const AssetImage('assets/images/profile_user_photo.png'),
-                            fit: BoxFit.cover,
-                          ),
-                          borderRadius:
-                              BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
-                        ),
-                      ),
-                    ],
-                  ),
+Widget build(BuildContext context) {
+  return FutureBuilder<String?>(
+    future: userEmail,
+    builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return CircularProgressIndicator();
+      } else if (snapshot.hasError) {
+        return Text("Error: ${snapshot.error}");
+      } else {
+        return Column(
+          children: <Widget>[
+            Container(
+              width: 120.0,
+              height: 122.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage('assets/images/profile_user_photo.png'),
+                  fit: BoxFit.cover,
                 ),
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
               ),
-              
-              Pinned.fromPins(
-                Pin(start: 0.0, end: 0.0),
-                Pin(size: 19.0, end: 0.0),
-                child: Text(
-                  snapshot.data ?? 'Guest', // Displaying the user's email
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 14,
-                    color: const Color(0xff000000),
-                    height: 1.2142857142857142,
-                  ),
-                  textHeightBehavior:
-                      TextHeightBehavior(applyHeightToFirstAscent: false),
-                  softWrap: false,
-                ),
+            ),
+            SizedBox(height: 10), // you can adjust this as per your requirement
+            Text(
+              snapshot.data ?? 'Guest', // Displaying the user's email
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 14,
+                color: const Color(0xff000000),
+                height: 1.2142857142857142,
               ),
-            ],
-          );
-        }
-      },
-    );
-  }
+              textHeightBehavior:
+                  TextHeightBehavior(applyHeightToFirstAscent: false),
+              softWrap: false,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        );
+      }
+    },
+  );
+}
+
 }
